d6065110b0f247f7c9a9f01438a41d0aae450b59
mananuf <bmananaf@gmail.com>
2025-03-12 13:34:08 +0100

    adds logic for reading csv file and setting output to Vec<String>

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+use std::{error::Error, process};
+use std::path::Path;
+use std::fs;
+
 pub struct Config {
     pub command: String,
     pub file_path: String,
@@ -15,4 +19,21 @@ impl Config {
 pub fn parse_config(args: &[String]) -> Config {
     let config: Config = Config::new(&args);
     config
+}
+
+pub fn csv_reader<P: AsRef<Path>>(filename: P) -> Result<Vec<String>, Box<dyn Error>> {
+    let file = fs::File::open(filename)?;
+    let mut reader = csv::Reader::from_reader(file);
+    let mut data: Vec<String> = vec![];
+
+    for result in reader.records() {
+        let record = result?;
+        let record_to_string = record.iter().collect::<Vec<&str>>().join(",");
+        data.push(record_to_string);
+        
+        // println!("{record_to_string}");
+        // println!("{:?}", record);
+    }
+
+    Ok(data)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,20 @@
 use std::env;
-use std::fs;
 
 pub mod merkle;
 pub mod lib;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-
     let config = lib::parse_config(&args);
 
     run(config);
-    let data = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-    let merkle_tree = merkle::generate_merkle_tree(&data);
-    // println!("{:#?}", get_merkle_root(&merkle_tree));
-    let res: String = merkle::get_merkle_root(&merkle_tree);
-    println!("{res}");
 }
 
 fn run(config: lib::Config) {
-    let content = fs::read_to_string(&config.file_path).expect("error reading file");
+    let data = lib::csv_reader(&config.file_path);
+    let merkle_tree = merkle::generate_merkle_tree(&data.unwrap());
+    println!("{:#?}", &merkle_tree);
+
+    let merkle_root: String = merkle::get_merkle_root(&merkle_tree);
+    println!("{merkle_root}");
 }
